7b89fcea95938ef6b34495c19e99d8f096e9efb9
christos.karytsas <christos.karytas@gugroup.com>
2022-07-04 18:16:04 +0300

    adding firebase

--- app/src/main/java/com/example/barcodescanner/MainActivity.java
+++ app/src/main/java/com/example/barcodescanner/MainActivity.java
@@ -3,20 +3,45 @@ package com.example.barcodescanner;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 
 public class MainActivity extends AppCompatActivity {
 
+    // creating a variable for
+    // our Firebase Database.
+    FirebaseDatabase firebaseDatabase;
+
+    // creating a variable for our
+    // Database Reference for Firebase.
+    DatabaseReference databaseReference;
+
     Button btn_scan;
+    public String barcode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // below line is used to get the instance
+        // of our Firebase database.
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        // below line is used to get
+        // reference for our database.
+        databaseReference = firebaseDatabase.getReference("books");
+
         setContentView(R.layout.activity_main);
         btn_scan = findViewById(R.id.btn_scan);
         btn_scan.setOnClickListener(v -> {
@@ -36,16 +61,51 @@ public class MainActivity extends AppCompatActivity {
         barLauncher.launch(options);
     }
 
+    private void getdata(String scan) {
+
+        DatabaseReference scannedRef = databaseReference.child(scan);
+
+
+        System.out.println("this is my print" + scannedRef);
+        // calling add value event listener method
+        // for getting the values from database.
+        databaseReference.addValueEventListener(new ValueEventListener() {
+
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // this method is call to get the realtime
+                // updates in the data.
+                // this method is called when the data is
+                // changed in our Firebase console.
+                // below line is for getting the data from
+                // snapshot of our database.
+                // barcode = databaseReference.orderByChild("books").equalTo(scan);
+
+                // after getting the value we are setting
+                // our value to our text view in below line.
+                //barcode = scanned_book;
+                //System.out.println("this is my print" + scanned_book);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // calling on cancelled method when we receive
+                // any error or we are not able to get the data.
+                Toast.makeText(MainActivity.this, "Fail to get data.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
     ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result->
     {
         if(result.getContents() !=null)
         {
             AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
             builder.setTitle("Result");
-
+            getdata(result.getContents());
             //result.getContents = barcode
             //connect to database and find id = result.getContents and get price
-
             builder.setMessage(result.getContents());
             builder.setPositiveButton("OK", new DialogInterface.OnClickListener()
             {
