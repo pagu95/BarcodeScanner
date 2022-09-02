527c1d1c24bfd185ba59b1f1c761b12051ba9342
christos.karytsas <christos.karytas@gugroup.com>
2022-09-02 23:05:48 +0300

    FillDoc commit. Modify app to make apografi possible

--- app/src/main/java/com/example/barcodescanner/MainActivity.java
+++ app/src/main/java/com/example/barcodescanner/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.barcodescanner;
 
 import android.app.ProgressDialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.DefaultRetryPolicy;
@@ -20,15 +19,11 @@ import com.android.volley.toolbox.Volley;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 public class MainActivity extends AppCompatActivity {
 
     Button btn_scan;
     public String book_details;
-    public String url = "https://script.google.com/macros/s/AKfycbyTUIgpvf2ytNTWC-5_VlzIG5xFODwLTE5_29AntcNieZmvTW-RhN3lHNv_CiEgBfr0TQ/exec?action=getItems&isbn=";
+    public String url = "https://script.google.com/macros/s/AKfycbw6fKtTfEqxQeE7GU8tQt247x4PRm46WhVQdPWoIPoJzFX8bcArDN6mv52XvnJz35H-Hw/exec?action=addItem&isbn=";
     ProgressDialog loading;
 
     @Override
@@ -53,58 +48,28 @@ public class MainActivity extends AppCompatActivity {
         barLauncher.launch(options);
     }
 
-    public void showDetails(String book_details){
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setTitle("Result");
-        builder.setMessage(book_details);
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.dismiss();
-            }
-        }).show();
-    }
-
-    private String fetchDetails(String bookString) {
-        String title = null;
-        String price = null;
-        String supplier = null;
-        String details = "Book Not Found";
-        try {
-
-            if (!bookString.contains("[]")) {
-
-                JSONObject bookJson = new JSONObject(bookString);
-                JSONArray barcode = bookJson.getJSONArray("barcodes");
-                title = barcode.getJSONObject(0).getString("title");
-                price = Double.toString(barcode.getJSONObject(0).getDouble("price"));
-                supplier = barcode.getJSONObject(0).getString("supplier");
-
-                details = title + " ΤΙΜΗ: " + price + " ΠΡΟΜΗΘΕΥΤΗΣ: " + supplier;
-
-            } else {
-                System.out.println("THIS IS THE ELSE RESULT ->" + bookString);
-            }
-
-        } catch (JSONException e) {
-            e.printStackTrace();
+    public void showDetails(String response,String isbn){
+        String toastText;
+        if (response.equals("Successfull")){
+            toastText = response + "call for isbn :" + isbn;
+        }else{
+            toastText = "Error for isbn :" + isbn;
         }
-        return details;
+        Toast.makeText(this,toastText, Toast.LENGTH_SHORT).show();
     }
 
-    private void getdata(String scan) {
+    private void sendData(String scan) {
 
         String book_url = url + scan;
         System.out.println("THIS IS THE URL I HIT -> " + book_url);
 
         RequestQueue queue = Volley.newRequestQueue(this);
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, book_url,
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, book_url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         System.out.println("RESPONSE" + response);
-                        book_details = fetchDetails(response);
-                        showDetails(book_details);
+                        showDetails(response,scan);
                         loading.dismiss();
                     }
                 },
@@ -128,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
     ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result ->
     {
         if (result.getContents() != null) {
-            getdata(result.getContents());
+            sendData(result.getContents());
             loading = ProgressDialog.show(this,"Loading","please wait",false,true);
         }
     });
